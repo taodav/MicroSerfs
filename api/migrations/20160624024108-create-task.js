b64f1ec3ea890b1e97477016be820e8bb8d8b4d8
David Tao <ruoyutao@gmail.com>
2016-06-24 17:09:44 +0800

    Add tasks and users migrations

---
+++ api/migrations/20160624024108-create-task.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Tasks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      price: {
+        type: Sequelize.INTEGER
+      },
+      complete: {
+        defaultValue: false,
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Tasks');
+  }
+};
