554bb488881e57d68bcd18b44842ab19a25007b8
David Tao <ruoyutao@gmail.com>
2016-06-25 21:19:35 +0800

    Add routes for serfs

---
+++ api/routes/serfs.js
@@ -0,0 +1,31 @@
+import {Router} from 'express'
+import models from '../models';
+import tasks from './tasks'
+
+export default () => {
+	let app = Router({mergeParams: true})
+
+	app.get('/new', (req, res) => {
+		res.render('serfs/new')
+	})
+
+	app.get('/:id', (req, res) => {
+		models.Serf.find({
+			where: {
+				id: req.params.id
+			},
+			include: [models.Task]	
+		}).then((serf) => {
+			res.render('serfs/show', {serf})
+		})
+	})
+
+	app.post('/', (req, res) => {
+		models.Serf.create(req.body.serf).then((serf) => {
+			req.session.serfId = serf.dataValues.id
+			req.app.locals.serfId = serf.dataValues.id
+			res.redirect('/');
+		})
+	})
+	return app
+}
