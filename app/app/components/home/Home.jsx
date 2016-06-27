0919b897f518f8ea5c2026c1a168d1288e4344a1
David Tao <ruoyutao@gmail.com>
2016-06-27 12:06:03 +0800

    Refactor for registered and unregistered users

--- app/app/components/home/Home.jsx
+++ app/app/components/home/Home.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import UnregisteredHome from './UnregisteredHome.jsx'
+import RegisteredUserHome from './RegisteredUserHome.jsx'
+import auth from '../../utils/auth.jsx'
 
 export default class Home extends React.Component {
+	constructor(){
+		super();
+		this.state = {
+			user: null
+		}
+	}
+	componentWillMount(){
+		if (auth.loggedIn()){
+			auth.getUser(localStorage.id).then((res) => {
+				this.setState({user: res.data.user})
+			})
+		}
+	}
 	render(){
-		return (
-			<UnregisteredHome />
-		)
+		if (auth.loggedIn()){
+			return (
+				<RegisteredUserHome user={this.user} />
+			)
+		} else {
+			return (
+				<UnregisteredHome />
+			)
+		}
 	}
 }
