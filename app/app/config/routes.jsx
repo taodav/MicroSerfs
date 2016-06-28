73a71bd6328a43298814e3513e4c49c8a091d11b
David Tao <ruoyutao@gmail.com>
2016-06-28 23:14:08 +0800

    Add new task route

--- app/app/config/routes.jsx
+++ app/app/config/routes.jsx
@@ -3,6 +3,7 @@ import Main from '../components/Main.jsx'
 import CreateUser from '../components/user/CreateUser.jsx'
 import Home from '../components/home/Home.jsx'
 import Login from '../components/user/Login.jsx'
+import CreateTask from '../components/tasks/CreateTask.jsx'
 import {Route, IndexRoute} from 'react-router'
 
 const CreateUserWrapper = () => <CreateUser myType={"users"} />
@@ -12,6 +13,9 @@ const CreateSerfWrapper = () => <CreateUser myType={"serfs"} />
 const routes = () =>
 		<Route path="/" component={Main}>
 			<Route path="users/new" component={CreateUserWrapper} />
+			<Route path="users/:id">
+				<Route path="/tasks/new" component={CreateTask} />
+			</Route>
 			<Route path="serfs/new" component={CreateSerfWrapper} />
 			<Route path="sessions/new" component={Login} />
 			<IndexRoute component={Home} />
