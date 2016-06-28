fb6deb6bddf597ad1c6d894f6b270fab6ddb3ec5
David Tao <ruoyutao@gmail.com>
2016-06-28 23:15:42 +0800

    Refactor navbar into multiple components

---
+++ app/app/components/navbar/Nav.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Link, hashHistory} from 'react-router'
+import NavLinks from './NavLinks.jsx'
+
+export default class Nav extends React.Component {
+	
+	render(){
+		return (
+			<nav className="navbar navbar-inverse">
+				<div className="container">
+					<div className="navbar-header">
+						<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+							<span className="sr-only">Toggle navigation</span>
+							<span className="icon-bar"></span>
+							<span className="icon-bar"></span>
+							<span className="icon-bar"></span>
+						</button>
+						<Link to="/" className="navbar-brand">MicroSerfs</Link>
+					</div>
+					<div id="navbar" className="collapse navbar-collapse">
+						<ul className="nav navbar-nav">
+							<NavLinks />
+						</ul>
+					</div>
+				</div>
+			</nav>
+		)
+	}
+}
