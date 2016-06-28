714aee31c750fab1e691d7ca15e90e32faa6475c
David Tao <ruoyutao@gmail.com>
2016-06-28 10:44:25 +0800

    Update to include correct info when mounting component

--- app/app/components/home/RegisteredUserHome.jsx
+++ app/app/components/home/RegisteredUserHome.jsx
@@ -3,16 +3,15 @@ import {Link} from 'react-router'
 import auth from '../../utils/auth.jsx'
 
 export default class RegisteredUserHome extends React.Component {
-	componentWillMount() {
-		auth.getUser(localStorage.id).then((res) => {
-			console.log(res.data)
-			this.user = res.data.user
-		})
-	}
+	// componentWillMount() {
+	// 	auth.getUser(localStorage.id).then((res) => {
+	// 		this.user = res.data.user
+	// 	})
+	// }
 	render(){
 		return(
 			<div className="starter-template">
-				<h1>Welcome Back, {this.user.first_name}</h1>
+				<h1>Welcome Back, {this.props.user.first_name}</h1>
 		        <p className="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
 	        </div>
 		)
