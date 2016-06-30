afa613b0e556fb941fa1e28d2653e6e9043910e8
David Tao <ruoyutao@gmail.com>
2016-06-30 19:31:25 +0800

    Add Routes for components in create task

--- app/app/config/routes.jsx
+++ app/app/config/routes.jsx
@@ -5,6 +5,8 @@ import Home from '../components/home/Home.jsx'
 import Login from '../components/user/Login.jsx'
 import CreateTask from '../components/tasks/CreateTask.jsx'
 import {Route, IndexRoute} from 'react-router'
+import TaskForm from '../components/tasks/TaskForm.jsx'
+import SearchSerf from '../components/tasks/SearchSerf.jsx'
 
 const CreateUserWrapper = () => <CreateUser myType={"users"} />
 const CreateSerfWrapper = () => <CreateUser myType={"serfs"} />
@@ -16,7 +18,10 @@ const routes = () =>
 		<Route path="users/new" component={CreateUserWrapper} />
 
 		// New Task
-		<Route path="users/:id/tasks/new" component={CreateTask} />
+		<Route path="users/:id/tasks/new" component={CreateTask}>
+			<Route path="/search" component={SearchSerf}>
+			<IndexRoute component={TaskForm} />
+		</Route>
 
 		// New Serf
 		<Route path="serfs/new" component={CreateSerfWrapper} />
