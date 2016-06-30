9415044cb84efedebacc8b9f692334327ac1b897
David Tao <ruoyutao@gmail.com>
2016-06-30 13:54:55 +0800

    Add link to new task

--- app/app/components/home/RegisteredUserHome.jsx
+++ app/app/components/home/RegisteredUserHome.jsx
@@ -3,17 +3,13 @@ import {Link} from 'react-router'
 import auth from '../../utils/auth.jsx'
 
 export default class RegisteredUserHome extends React.Component {
-	// componentWillMount() {
-	// 	auth.getUser(localStorage.id).then((res) => {
-	// 		this.user = res.data.user
-	// 	})
-	// }
+
 	render(){
 		return(
 			<div className="starter-template">
 				<h1>Welcome Back, {this.props.user.first_name}</h1>
 		        <p className="lead">Get something done today.</p>
-		        <Link to="/task/new" className="btn btn-primary">Create A Task</Link>
+		        <Link to={"/users/" + this.props.user.id + "/tasks/new"} className="btn btn-primary">Create A Task</Link>
 	        </div>
 		)
 	}
